b42b01411ce09fb3ef486af1dff2cd81d0d982cb
kaite <kaite@pplabs.org>
2023-09-28 03:56:08 +0000

    update litelama version to 0.1.7

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ import launch
 # TODO: add pip dependency if need extra module only on extension
 
 if not launch.is_installed("litelama"):    
-    launch.run_pip("install litelama=0.1.6")
-
+    launch.run_pip("install litelama==0.1.7")
+    
